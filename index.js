10fd08d53d099ee18959af243fffc498bd7a63f4
Jai Babbar <jaibabbar08@gmail.com>
2022-03-12 23:51:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let count = 0;
+
+function increment() {
+    count++;
+    document.getElementById("count-el").innerText = count;
+}
+
+function save() {
+    let countStr = count + " - ";
+    document.getElementById("save-el").textContent += countStr;
+    count = 0;
+    document.getElementById("count-el").innerText = count;
+}
